f2c28576422207c80fdf188c5280089b78aab985
cubalibbre <202319810004@ifba.edu.br>
2024-08-21 10:19:12 -0300

    Create listalp1.py

---
+++ listalp1.py
@@ -0,0 +1,38 @@
+# #1
+# for i in range(100):
+#     print("Dante")
+# #2
+# nome=input("Insira o seu nome:")
+# for i in range(100):
+#     print(nome)
+# #3
+# nome=input("Insira o seu nome:")
+# qtd=int(input("Digite quantas vezes o nome será impresso:"))
+# for i in range(qtd):
+#     print(nome)
+# #4
+# nome=input("Insira o seu nome:")
+# qtd=int(input("Digite quantas vezes o nome será impresso:"))
+# for i in range(qtd):
+#     print(i, nome)
+# #5
+# nome=input("Insira o seu nome:")
+# qtd=int(input("Digite quantas vezes o nome será impresso:"))
+# for i in range(1, qtd+1):
+#     print(nome, i,"° vez")
+# #6
+# inicio=int(input("Insira o numero inicial:"))
+# qtd=int(input("Digite a quantidade de numeros impressos:"))
+# for i in range(qtd):
+#     print(inicio+i*2)
+# #7
+# inicio=int(input("Insira o numero inicial:"))
+# qtd=int(input("Digite a quantidade de numeros impressos:"))
+# for i in range(qtd):
+#     print(inicio-i*2)
+# #8
+# num=int(input("Insira o numero inicial: "))
+# qtd=int(input("Digite quantos números serão impressos: "))
+# varia=int(input("Insira a variação:"))
+# for i in range(qtd, varia):
+#     print(i)
