e5a9f9a2baa127b44e78b259a19661611cddc4ad
cubalibbre <202319810004@ifba.edu.br>
2024-08-21 09:22:52 -0300

    Add files via upload

---
+++ dante.py
@@ -0,0 +1,17 @@
+# #1
+# for i in range(100):
+#     print("Dante")
+# #2
+# nome=input("Insira o seu nome:")
+# for i in range(100):
+#     print(nome)
+# #3
+# nome=input("Insira o seu nome:")
+# qtd=int(input("Digite quantas vezes o nome será impresso:"))
+# for i in range(qtd):
+#     print(nome)
+# #4
+# nome=input("Insira o seu nome:")
+# qtd=int(input("Digite quantas vezes o nome será impresso:"))
+# for i in range(qtd):
+#     print(i, nome)
